087c709777a18f0b8ea82f39a35bfaf74132fd35
Dirisu <dirisumarvellous94@gmail.com>
2022-08-17 11:53:45 +0100

    set up a basic collection view with compositional lay out

---
+++ CollectionViewCompositionalLayout/CompositionalLayout.swift
@@ -0,0 +1,62 @@
+//
+//  CompositionalLayout.swift
+//  CollectionViewCompositionalLayout
+//
+//  Created by Marvellous Dirisu on 17/08/2022.
+//
+
+import UIKit
+
+enum CompositionalGroupAlignment {
+    case horizontal
+    case vertical
+}
+
+struct CompositionalLayout {
+    
+    static func createItem(width: NSCollectionLayoutDimension,
+                           height: NSCollectionLayoutDimension,
+                           spacing: CGFloat
+    ) -> NSCollectionLayoutItem {
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: width, heightDimension: height))
+        item.contentInsets = NSDirectionalEdgeInsets(top: spacing, leading: spacing, bottom: spacing, trailing: spacing)
+        
+        return item
+                                          
+    }
+    
+    static func createGroup(alignment: CompositionalGroupAlignment,
+                            width: NSCollectionLayoutDimension,
+                            height: NSCollectionLayoutDimension,
+                            items: [NSCollectionLayoutItem]
+    ) -> NSCollectionLayoutGroup {
+        
+        switch alignment {
+            
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width, heightDimension: height), subitems: items)
+            
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width, heightDimension: height), subitems: items)
+        
+        }
+    }
+    
+    static func createGroup(alignment: CompositionalGroupAlignment,
+                            width: NSCollectionLayoutDimension,
+                            height: NSCollectionLayoutDimension,
+                            item: NSCollectionLayoutItem,
+                            count: Int
+    ) -> NSCollectionLayoutGroup {
+        
+        switch alignment {
+            
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width,heightDimension: height), subitem: item, count: count)
+            
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width, heightDimension: height), subitem: item, count: count)
+        
+        }
+    }
+}
--- CollectionViewCompositionalLayout/ViewController.swift
+++ CollectionViewCompositionalLayout/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let images: [UIImage] = Array(1...11).map {UIImage (named: String($0))! }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.collectionViewLayout = createLayout()
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        // item
+//        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+//
+//        // add paddings
+//        item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        let item = CompositionalLayout.createItem(width: .fractionalWidth(0.5), height: .fractionalHeight(1), spacing: 1)
+        
+        let fullItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(1), spacing: 1)
+        
+        // group
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.3)), subitem: item, count: 2)
+        
+        let verticalGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(0.5), height: .fractionalHeight(1), item: fullItem, count: 2)
+        
+        let horizontalGroup = CompositionalLayout.createGroup(alignment: .horizontal, width: .fractionalWidth(1), height: .absolute(300), items: [item, verticalGroup])
+        
+        // sets the vertical alignment to a full width
+        let mainItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .absolute(200), spacing: 1)
+        let mainGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(1), height: .absolute(500), items: [mainItem, horizontalGroup])
+        
+        // section
+        let section = NSCollectionLayoutSection(group: mainGroup)
+        
+        // return
+        return UICollectionViewCompositionalLayout(section: section)
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
+        cell.setUp(image: images[indexPath.row])
+        
+        return cell
+    }
+}
+
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionImage: UIImageView!
+    
+    func setUp(image: UIImage){
+        collectionImage.image = image
+    }
+}
